847dc28e91dc9580fedc858c115ce56dc26fcbc3
GabrielaSevcikova <gabikasevcikova9@gmail.com>
2025-03-21 19:11:37 +0100

    Add files via upload

---
+++ parts/footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upright</title>
+
+<!--Footer-->
+<footer>
+<div class="tm-section-wrap tm-copyright row">
+    <div class="col-12">
+        <div class="text-right">
+            <p>Copyright 2020 Upright Company</p>
+            <a href="#terms">Terms of Service</a> | <a href="#privacy">Privacy Policy</a>
+        </div> 
+    </div>
+</div>
+</footer>
